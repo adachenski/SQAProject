0ad69d529b8ec43cb209d080a135375271ba4570
nasko <adachenski@aol.com>
2017-08-25 21:05:08 -0700

    random testTypes and documentation question from the server send to ejs view

--- SQA-Project/index.js
+++ SQA-Project/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
- var path = require('path');
+var path = require('path');
 var port = process.env.PORT || 3000;
 var homeRouter = require('./src/routes/homeRouter');
 var sqaQuestions2 = require('./public/json/portnovQuestions2');
@@ -9,10 +9,10 @@ var questionRouter = require('./src/routes/questionRouter')(sqaQuestions2);
 var bodyParser = require('body-parser');
 
 var rootPath = path.normalize(__dirname + '/');
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(rootPath+'public'));
-app.set('views', path.join(rootPath+'src/views'));
+app.use(express.static(rootPath + 'public'));
+app.set('views', path.join(rootPath + 'src/views'));
 
 // var fs = require('fs');
 // fs.readFile('./public/json/portnovQuestions2.json', function (err, content) {
@@ -24,7 +24,7 @@ app.set('views', path.join(rootPath+'src/views'));
 //             var x = newJson[i]["content"].replace(/;/g,";</br>");
 //             newJson[i]["content"] = x;
 //         }
-        
+
 //     }
 //     fs.writeFile('./public/json/portnovQuestions3.json', JSON.stringify(newJson), function (err) {
 //         if (err) throw err;
@@ -38,18 +38,47 @@ function getDetailCategory(allQuestions, category) {
             tempArr.push(allQuestions[i])
         }
     }
-    return tempArr
+    return tempArr;
 
 };
+function getRandomQuestions(arr, target) {
+    var randomArr = [];
+    for (var y = 0; y < target; y += 1) {
+        var randNumber = Math.floor(Math.random() * arr.length);
+        var randQuestion = arr[randNumber];
+        var found = false;
+        if (y == 0) {
+            randomArr.push(randQuestion);
+            continue;
+        }
+
+        for (var z = 0; z < randomArr.length; z += 1) {
+            if (randQuestion.id == randomArr[z].id) {
+                found = true;
+                y--;
+            }
+        }
+        if (!found) {
+            randomArr.push(randQuestion);
+            found = false;
+        }
+    }
+    return randomArr;
+}
 app.set('view engine', 'ejs');
 
-app.set('views','./src/views');
+app.set('views', './src/views');
 app.get('/', function (req, res) {
-       var allQuestions = detailQuestions;
-            var testingTypes = getDetailCategory(allQuestions, 'testingTypes');
-            console.log(testingTypes);
+    var allQuestions = detailQuestions;
+    var testingTypes = getDetailCategory(allQuestions, 'testingTypes');
+    var randomTestingTypes = getRandomQuestions(testingTypes, 3);
+    var documentation = getDetailCategory(allQuestions, 'documentation');
+    var randomDocumentation = getRandomQuestions(documentation, 3);
+    console.log(randomDocumentation);
     res.render('index', {
         title: "Index",
+        testingTypes: randomTestingTypes,
+        documentation: randomDocumentation
     });
 });
 
@@ -76,4 +105,3 @@ app.listen(port, function (err) {
 //         }
 //     }
 //     return finalQuestions;
-// }
